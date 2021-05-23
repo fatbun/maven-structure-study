add455065e20fb1101c622f3eb95b34b0aa34445
lb <284634275@qq.com>
2021-05-23 14:49:01 +0800

    first commit

---
+++ myproject3/src/main/java/com/benjamin/myproject3/Myproject3Application.java
@@ -0,0 +1,13 @@
+package com.benjamin.myproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Myproject3Application.class, args);
+    }
+
+}
---
+++ myproject3/src/test/java/com/benjamin/myproject3/Myproject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.myproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myproject3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
